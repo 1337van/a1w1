d24eef674fc61611547d8438b8c5ff13af8f1f0d
1337van <158235698+1337van@users.noreply.github.com>
2025-04-23 22:43:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ UPLOAD_PREFIX = "input/A1W1APP"
 
 # --- AUTHENTICATION via Secrets ---
 service_account_info = dict(st.secrets["service_account"])
-service_account_info["private_key"] = service_account_info["private_key"].replace("\n", "
-").strip()
+service_account_info["private_key"] = service_account_info["private_key"].replace("\\n", "\n").strip()
+
 credentials = service_account.Credentials.from_service_account_info(
     service_account_info,
     scopes=["https://www.googleapis.com/auth/cloud-platform"]
